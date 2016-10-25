2da003b071e31be06aff0286a9c4d7dc127fcb40
yuxiaomao <0210youjing@gmail.com>
2016-10-25 15:03:43 +0200

    Changement timer_1234
    plusieur pointeur fonc, passer en.c, enleve enable coté APB pont

--- pilotes/inc/timer_1234.h
+++ pilotes/inc/timer_1234.h
@@ -12,9 +12,11 @@
 #include "clock.h" 
 #include "gpio.h"
 
+/*Il faut faire example avant utilisation Timer:
+* RCC->APB2ENR |= RCC_APB2ENR_TIM1EN ;
+*/
 u32 Timer_1234_Init(TIM_TypeDef *Timer, u32 Frequence ) ;
 void Timer_Active_IT(TIM_TypeDef *Timer, u8 Priority, void (*IT_function) (void)); 
 
-void (* ptr_fonc) (void) ;
 
 #endif 
--- pilotes/src/timer_1234.c
+++ pilotes/src/timer_1234.c
@@ -1,10 +1,9 @@
-/**
-  ******************************************************************************
-  * @file    Timer_1234.c
-  * @author  Quentin Leclerc & Nathan Balblanc
-  * @date    16-September-2016
-**/
 #include "Timer_1234.h"
+
+void (* ptr_fonc1) (void) ;
+void (* ptr_fonc2) (void) ;
+void (* ptr_fonc3) (void) ;
+void (* ptr_fonc4) (void) ;
 
 /** 
 * Configure les Timers 1, 2, 3 et 4
@@ -20,33 +19,12 @@ u32 Timer_1234_Init(TIM_TypeDef *Timer, u32 Frequence ) {
 	
 	u32 Clock, psc, arr , freq_interne;
 	
-	 // Enable du Timer concerné par le Init 
-   if (Timer == TIM1) 
-   {
-         RCC->APB2ENR |= RCC_APB2ENR_TIM1EN ;
-   }
-   else if (Timer == TIM2)   
-   {  
-        RCC->APB1ENR |= RCC_APB1ENR_TIM2EN ;    
-   }
-   else if (Timer == TIM3)   
-   {  
-        RCC->APB1ENR |= RCC_APB1ENR_TIM3EN ;    
-   }
-   else if (Timer == TIM4)   
-   {  
-        RCC->APB1ENR |= RCC_APB1ENR_TIM4EN ;    
-   }  
    Timer->CR1 |= TIM_CR1_CEN ;
 	
 	Clock = CLOCK_GetTIMCLK(Timer) ; 
-	// (1 << 16) = 65 536 = valeur max de la division Arr pour le maximiser
-
- 	
 	// Ex : 72 000 000 / 125 = 576 000 
 	// 576 000 / 65 536 = 8,7 = 8 en entier
 	// donc on veut 9, car avec 8 on aurait pas assez
-	
 	// Si ça tombe pas pile, alors on ajoute 1 
 	// car en dessous c'est pas assez ! 
    freq_interne = Clock/Frequence;
@@ -75,51 +53,52 @@ TIM_TypeDef
 d'interruption
 **/
 void Timer_Active_IT(TIM_TypeDef *Timer, u8 Priority, void (*IT_function) (void)) {
-   ptr_fonc = IT_function ;
+  
    
    if (Timer == TIM2) 
       {
          NVIC->ISER[0] |= NVIC_ISER_SETENA_28 ;
          NVIC->IP[28] |= Priority << 4 ;
+				 ptr_fonc2 = IT_function ;
       }
    else if (Timer == TIM3) 
       { 
          NVIC->ISER[0] |= NVIC_ISER_SETENA_29 ;
          NVIC->IP[29] |= Priority << 4 ;
+				 ptr_fonc3 = IT_function ;
       }
    else if (Timer == TIM4) 
       {
          NVIC->ISER[0] |= NVIC_ISER_SETENA_30 ;
          NVIC->IP[30] |= Priority << 4 ;
+				 ptr_fonc4 = IT_function ;
       }
    else 
       {
          NVIC->ISER[0] |= NVIC_ISER_SETENA_25 ;
          NVIC->IP[25] |= Priority << 4 ;
+				 ptr_fonc1 = IT_function ;
       }
       Timer->DIER |= TIM_DIER_UIE ;
 }
 
 
 void TIM1_IRQHandler (void) {
-   ptr_fonc() ;
+   ptr_fonc1() ;
    TIM1->SR &= ~(TIM_SR_UIF) ;
 }
 
 void TIM2_IRQHandler (void) {
-   ptr_fonc() ;
+   ptr_fonc2() ;
    TIM2->SR &= ~(TIM_SR_UIF) ;
 }
 
 void TIM3_IRQHandler (void) {
-   ptr_fonc() ;
+   ptr_fonc3() ;
    TIM3->SR &= ~(TIM_SR_UIF) ;
 }
 
 void TIM4_IRQHandler (void) {
-   ptr_fonc() ;
+   ptr_fonc4() ;
    TIM4->SR &= ~(TIM_SR_UIF) ;
 }
-
-
-
