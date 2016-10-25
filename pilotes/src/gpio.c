c547fd1d77cce6789f8af12f3778cf12e5aa2da4
yuxiaomao <0210youjing@gmail.com>
2016-10-25 11:12:46 +0200

    Changement pour un pilotes compilable
    Ameliorer les notation (le master a bcp trop de erreur?)

--- pilotes/src/gpio.c
+++ pilotes/src/gpio.c
@@ -1,129 +1,50 @@
-// TRAVAIL DE LOIS LANO et RAMA DESPLATS
-// gpio.h
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @author  Quentin Leclerc & Nathan Balblanc
+  * @date    16-September-2016
+**/
+#include "gpio.h" 
 
-#include "gpio.h"
 
-void Port_IO_Init(GPIO_TypeDef* port) {
-    if (port == GPIOA) {
-        RCC->APB2ENR = RCC_APB2ENR_IOPAEN;
-    } else if (port == GPIOB) {
-        RCC->APB2ENR = RCC_APB2ENR_IOPBEN;
-    } else if (port == GPIOC) {
-        RCC->APB2ENR = RCC_APB2ENR_IOPCEN;
-    } else if (port == GPIOD) {
-        RCC->APB2ENR = RCC_APB2ENR_IOPDEN;
-    }
-}
-
-void Port_IO_Init_Output(GPIO_TypeDef* port, u8 broche) {
-	int bit = (broche%8)*4;
-	Port_IO_Init(port);
-	
-	if (broche < 8) {
-		port->CRL &= ~(0xF << bit);
-		port->CRL |= 1 << bit;
-	} else {
-		port->CRH &= ~(0xF << bit);
-		port->CRH |= 1 << bit;
+char Port_IO_Init_Output( GPIO_TypeDef * Port, u8 Broche) {	 
+	if (Broche <= 7) {
+		Port->CRL &=  ~(0xF << Broche*4) ;
+		Port->CRL |= (0x01 << Broche*4) ; 	
 	}
-}
-
-void Port_IO_Init_Input(GPIO_TypeDef* port, u8 broche) {
-	int bit = (broche%8)*4;
-	Port_IO_Init(port);
-	
-	if (broche < 8) {
-		port->CRH &= ~(0xF << bit);
-		port->CRL |= 1 << (bit+2);
-	} else {
-		port->CRH &= ~(0xF << bit);
-		port->CRH |= 1 << (bit+2);
+	else {
+		Broche -= 8 ;
+		Port->CRH = Port->CRH & ~(0xF << (Broche*4)) ;
+		Port->CRH = Port->CRH | (0x01 << (Broche*4)) ; 	
 	}
+	return 0 ;
 }
 
-void Port_IO_Set(GPIO_TypeDef* port, u8 broche) {
-	port->BSRR = 1 << broche;
+char Port_IO_Init_Input( GPIO_TypeDef * Port, u8 Broche) {
+	if (Broche <= 7) {
+		Port->CRL &= ~(0xF << (Broche*4)) ;
+		Port->CRL |= (0x01 << ((Broche*4)+2)) ; 	
+	}
+	else {
+		Broche-= 8 ;
+		Port->CRL = Port->CRH & ~(0xF << (Broche*4)) ;
+		Port->CRL = Port->CRH | (0x01 << ((Broche*4)+2)) ; 	
+	}
+	return 0 ;
 }
 
-void Port_IO_Reset(GPIO_TypeDef* port, u8 broche) {
-	port->BRR = 1 << broche;
+void Port_IO_Set(GPIO_TypeDef * Port, u8 Broche) {
+	Port->ODR = Port->ODR & (0x01 << Broche) ;
+} 
+
+void Port_IO_Reset(GPIO_TypeDef * Port, u8 Broche) {
+	Port->BSRR = Port->BSRR & (0x01 << (Broche+16) ) ;
 }
 
-void Port_IO_Blink(GPIO_TypeDef* port, u8 broche) {
-	port->ODR ^= 1 << broche;
+void Port_IO_Blink(GPIO_TypeDef * Port, u8 Broche) {
+   Port->ODR ^= (0x01 << Broche);
 }
 
-unsigned int Port_IO_Read(GPIO_TypeDef* port, u8 broche) {
-	return (port->IDR >> broche) & 1;
+unsigned int Port_IO_Read(GPIO_TypeDef * Port, u8 Broche) {
+	return (Port->IDR >> Broche) & 0x01 ;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#include "gpio.h"
-
-void Port_IO_Init(GPIO_TypeDef* port){
-    if(port == GPIOA)
-        RCC->APB2ENR |= RCC_APB2_ENR_IOPAEN;
-    else if(port == GPBIOB)
-        RCC->APB2ENR |= RCC_APB2_ENR_IOPBEN;
-    else
-        RCC->APB2ENR |= RCC_APB2_ENR_IOPCEN;
-}
-
-void Port_IO_Init_Output(GPIO_TypeDef* port, u8 broche){
-    int bit = (broche%8)*4; //modulo les 8 bits par CRL/CRH *4 bits si bit 1
-    if(bit < 8){
-        port->CRL &= ~(0xF) << bit; //car 0xF = 1111
-        port->CRL |= (1<<bit);
-    }else {
-        port->CRH &= ~(0xF) << bit; //car 0xF = 1111
-        port->CRH |= (1<<bit);
-    }
-}
-        
-void Port_IO_Init_Input(GPIO_TypeDef* port, u8 broche){
-    int bit = (broche%8)*4;
-    if (bit<8){
-        port->CRL &= ~(0xF) << bit;
-        port->CRL |= (1 << (bit+2);
-    }else{
-        port->CRL &= ~(0xF) << bit;
-        port->CRL |= (1 << (bit+2);
-    }
-}
-
-void Port_IO_Set(GPIO_TypeDef* port, u8 broche){
-    port->A
--- pilotes/src/timer_1234.c
+++ pilotes/src/timer_1234.c
@@ -1,86 +1,125 @@
-//__________________________________________________________
-// BINOME : LANO - DESPLATS
-// ETAT : 
-//__________________________________________________________
-
-//____ registres de périphériques du stm_32
-#include "stm32f10x.h"
-#include "clock.h"
-#include "gpio.h"
-#include "timer_1234.h"
-
-Timer_Handler timer_handler[4];
-
 /**
-* Configure la routine d'interruption d'un timer
-* @param timer Pointeur vers le jeu de registres (de type TIM_TypeDef ) du
-* timer considéré
-* @param priority Niveau de priorite´ de l'interruption
-* @param IT_function Pointeur sur la fonction qui sera exe´cute´e dans le routine d'interruption
+  ******************************************************************************
+  * @file    Timer_1234.c
+  * @author  Quentin Leclerc & Nathan Balblanc
+  * @date    16-September-2016
 **/
-void Timer_Active_IT(TIM_TypeDef *timer, u8 priority, Timer_Handler handler) {
-    if (timer == TIM1){
-		NVIC->ISER[0] |= NVIC_ISER_SETENA_25;
-		NVIC->IP[25] = priority << 4;
-        timer_handler[0] = handler;
-    } else if (timer == TIM2){
-		NVIC->ISER[0] |= NVIC_ISER_SETENA_28;
-		NVIC->IP[28] = priority << 4;
-        timer_handler[1] = handler;
-	} else if (timer == TIM3){
-		NVIC->ISER[0] |= NVIC_ISER_SETENA_29;
-		NVIC->IP[29] = priority << 4;
-        timer_handler[2] = handler;
-	} else if (timer == TIM4){
-		NVIC->ISER[0] |= NVIC_ISER_SETENA_30;
-		NVIC->IP[30] = priority << 4;
-        timer_handler[3] = handler;
-	}
-    
-	timer->DIER |= TIM_DIER_UIE;
-}
+#include "Timer_1234.h"
 
-u32 Timer_1234_Init(TIM_TypeDef *timer, u32 duree_us) {
-	CLOCK_Configure();
-    
-	//Activer les clocks
-    if (timer == TIM1){
-		RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
-    } else if (timer == TIM2){
-		RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-	} else if (timer == TIM3){
-		RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
-	} else if (timer == TIM4){
-		RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
-	}
-    
-	//enable clock dans AP bridge 1
-	timer->CR1 |= TIM_CR1_CEN | TIM_CR1_DIR; //Chip Enable du timer et le mettre en décomptage
-
-	// CLOCK_GetTIMCLK(timer)*(duree_us/1000000) //permet de récupérer la fréq du clock AH
-	u32 nb_ticks = ((CLOCK_GetTIMCLK(timer)/1000000) * duree_us); //On divise par 1000000 car la durée est en us
-	timer->PSC = nb_ticks/(1 << 16); //valeur max d'ARR : 65536	
-	timer->ARR = nb_ticks/(timer->PSC+1); 
+/** 
+* Configure les Timers 1, 2, 3 et 4
+* @param Timer Pointeur vers le jeu de registres (de type 
+TIM_TypeDef
+ ) du 
+* timer considéré
+* @param Duree_us Intervalle de temps exprimé en µs entre
+* deux débordements successifs
+* @return Le durée véritable qui a été configurée
+**/
+u32 Timer_1234_Init(TIM_TypeDef *Timer, u32 Frequence ) {
 	
-	return timer->ARR*(timer->PSC+1); //durée véritablement configurée
+	u32 Clock, psc, arr , freq_interne;
+	
+	 // Enable du Timer concerné par le Init 
+   if (Timer == TIM1) 
+   {
+         RCC->APB2ENR |= RCC_APB2ENR_TIM1EN ;
+   }
+   else if (Timer == TIM2)   
+   {  
+        RCC->APB1ENR |= RCC_APB1ENR_TIM2EN ;    
+   }
+   else if (Timer == TIM3)   
+   {  
+        RCC->APB1ENR |= RCC_APB1ENR_TIM3EN ;    
+   }
+   else if (Timer == TIM4)   
+   {  
+        RCC->APB1ENR |= RCC_APB1ENR_TIM4EN ;    
+   }  
+   Timer->CR1 |= TIM_CR1_CEN ;
+	
+	Clock = CLOCK_GetTIMCLK(Timer) ; 
+	// (1 << 16) = 65 536 = valeur max de la division Arr pour le maximiser
+
+ 	
+	// Ex : 72 000 000 / 125 = 576 000 
+	// 576 000 / 65 536 = 8,7 = 8 en entier
+	// donc on veut 9, car avec 8 on aurait pas assez
+	
+	// Si ça tombe pas pile, alors on ajoute 1 
+	// car en dessous c'est pas assez ! 
+   freq_interne = Clock/Frequence;
+   psc = freq_interne/65536;
+   if ( (freq_interne % 65536) != 0) 
+   {
+         psc+= 1 ;
+   }
+   
+   arr = Clock / Frequence / (psc) ;
+  
+   Timer->PSC = psc - 1 ; 
+   Timer->ARR = arr - 1 ;
+
+   return Clock / (psc*arr)  ; 
 }
 
-void TIM1_IRQHandler(void) {
-	timer_handler[0]();
-	TIM1->SR &= ~TIM_SR_UIF;
+/** 
+* Configure la routine d'interruption d'un Timer
+* @param Timer Pointeur vers le jeu de registres (de type 
+TIM_TypeDef
+ ) du 
+* timer considéré
+* @param Priority Niveau de priorité de l'interruption
+* @param IT_function Pointeur sur la fonction qui sera exécutée dans le routine
+d'interruption
+**/
+void Timer_Active_IT(TIM_TypeDef *Timer, u8 Priority, void (*IT_function) (void)) {
+   ptr_fonc = IT_function ;
+   
+   if (Timer == TIM2) 
+      {
+         NVIC->ISER[0] |= NVIC_ISER_SETENA_28 ;
+         NVIC->IP[28] |= Priority << 4 ;
+      }
+   else if (Timer == TIM3) 
+      { 
+         NVIC->ISER[0] |= NVIC_ISER_SETENA_29 ;
+         NVIC->IP[29] |= Priority << 4 ;
+      }
+   else if (Timer == TIM4) 
+      {
+         NVIC->ISER[0] |= NVIC_ISER_SETENA_30 ;
+         NVIC->IP[30] |= Priority << 4 ;
+      }
+   else 
+      {
+         NVIC->ISER[0] |= NVIC_ISER_SETENA_25 ;
+         NVIC->IP[25] |= Priority << 4 ;
+      }
+      Timer->DIER |= TIM_DIER_UIE ;
 }
 
-void TIM2_IRQHandler(void) {
-	timer_handler[1]();
-	TIM2->SR &= ~TIM_SR_UIF;
+
+void TIM1_IRQHandler (void) {
+   ptr_fonc() ;
+   TIM1->SR &= ~(TIM_SR_UIF) ;
 }
 
-void TIM3_IRQHandler(void) {
-	timer_handler[2]();
-	TIM3->SR &= ~TIM_SR_UIF;
+void TIM2_IRQHandler (void) {
+   ptr_fonc() ;
+   TIM2->SR &= ~(TIM_SR_UIF) ;
 }
 
-void TIM4_IRQHandler(void) {
-	timer_handler[3]();
-	TIM4->SR &= ~TIM_SR_UIF;
+void TIM3_IRQHandler (void) {
+   ptr_fonc() ;
+   TIM3->SR &= ~(TIM_SR_UIF) ;
 }
+
+void TIM4_IRQHandler (void) {
+   ptr_fonc() ;
+   TIM4->SR &= ~(TIM_SR_UIF) ;
+}
+
+
+
