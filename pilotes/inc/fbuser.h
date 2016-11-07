e6a71e8900daeef5aedf6b49689fd97bb14dac21
yuxiaomao <0210youjing@gmail.com>
2016-11-07 15:16:55 +0100

    premier idee sur fbuser

--- pilotes/inc/fbuser.h
+++ pilotes/inc/fbuser.h
@@ -2,13 +2,32 @@
 #define FBUSER_H
 
 #include "STM32F10X.h"
+#include "pwm.h"
 
-/*ce feedback genere signal pwm sortie D5/TIM3_CH1/PB4
-* Num_Target: 0 pour non speciale, 1 2 reserve aux pieds, 3+ pour differents plaque
-* Niv_Vibration: 1 default, regler 0.~1. 
-* 
+/*ce feedback genere signal pwm sortie
+* Voie:
+    1 : TIM3_CH1/PB4 D5
+    2 : TIM3_CH2/PB5 D4
+* Num_target: (on fait rien dessus pour l'instant) 
+    0 pour non speciale, 
+    1 2 reserve aux pieds,
+    3 4 5 6 7 8 pour differents plaque
+* Niv_vibration: 
+    regler 0.~1., ie rapport_cyclique dans pwm
+
+* Besoin avant d'utiliser
+    RCC->APB1ENR |= RCC_APB2ENR_TIM3EN;
+    RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
+* Config realise
+    PB4 Alternate PP
+    PWM init avec TIM3 freq 10
+    Niv_Vibration intique niveau genere par pwm
 */
-void feedback_user ( char Num_target, float Niv_vibration ){};
+/*apres init
+* 
+*
+*/
+void Feedback_user_Init (char Voie, char Num_target, float Niv_vibration);
 
 
 
--- pilotes/src/fbuser.c
+++ pilotes/src/fbuser.c
@@ -1,6 +1,15 @@
 #include "fbuser.h" 
 
+void Feedback_user_Init (char Voie, char Num_target, float Niv_vibration){
+    if (Voie == 1){
+        Port_IO_Init_Alter_PP(GPIOB,4);
+    }else if (Voie == 2){
+        Port_IO_Init_Alter_PP(GPIOB,5);
+    }
+    
+    /*20kHz pour la resolution d'un fichier son*/
+    PWM_Init(TIM3,1,20000);
+    PWM_set_Cycle(TIM3,1,Niv_vibration);
 
-
-
+}
 
