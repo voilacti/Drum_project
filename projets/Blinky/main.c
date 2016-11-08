7a162280d2676d58f1023673be527832610e9e5f
yuxiaomao <0210youjing@gmail.com>
2016-11-08 17:16:16 +0100

    Modification main pour tester fbuser

--- projets/Blinky/main.c
+++ projets/Blinky/main.c
@@ -2,7 +2,7 @@
 
 //____ registres de périphériques du stm_32
 #include "stm32f10x.h"
-#include "pwm.h"
+#include "fbuser.h"
 #include "adc.h"
 
 u16 data_adc;
@@ -12,12 +12,13 @@ int main (void)
     RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
     RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
     Init_ADC_Single_Conv(ADC1, 1) ;
-    Port_IO_Init_Alter_PP(GPIOB,9);
-    PWM_Init(TIM4,4,5000);
+	
+	  Feedback_user_Init (4, 0);
+	
 		while (1){
 			data_adc=ADC_read_data(ADC1);/*0-0xFFF*/
 			intensite=(float)data_adc/0xFFF;/*0-1.*/
-			PWM_set_Cycle(TIM4,4,intensite);
+			Feedback_user_In(4, intensite);
     }
 }
 
