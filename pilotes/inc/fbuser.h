b666c05c756b7ea26bfd0ac641cd847d8dbcf681
yuxiaomao <0210youjing@gmail.com>
2016-11-08 17:16:02 +0100

    Ajouter fonction fbuser

--- pilotes/inc/fbuser.h
+++ pilotes/inc/fbuser.h
@@ -3,18 +3,30 @@
 
 #include "STM32F10X.h"
 #include "pwm.h"
+/*Example:
+    RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
+    RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
+    Init_ADC_Single_Conv(ADC1, 1) ;
+	  Feedback_user_Init (4, 0);// Voie4
+		while (1){
+			data_adc=ADC_read_data(ADC1);
+			intensite=(float)data_adc/0xFFF;
+			Feedback_user_In(4, intensite);
+    }
+*/
 
-/*ce feedback genere signal pwm sortie
+/*ce feedback active Pin en alternet PushPull
+* genere signal pwm sortie
 * Voie:
-    1 : TIM3_CH1/PB4 D5
-    2 : TIM3_CH2/PB5 D4
-* Num_target: (on fait rien dessus pour l'instant) 
+    1 : TIM3_CH1 PB4 D5
+    2 : TIM3_CH2 PB5 D4
+		3 : TIM4_CH3 PB8 D15/I2C 
+		4 : TIM4_CH4 PB9 D14/I2C
+* Num_target: 
+ (on fait rien dessus pour l'instant, 0 default) 
     0 pour non speciale, 
     1 2 reserve aux pieds,
     3 4 5 6 7 8 pour differents plaque
-* Niv_vibration: 
-    regler 0.~1., ie rapport_cyclique dans pwm
-
 * Besoin avant d'utiliser
     RCC->APB1ENR |= RCC_APB2ENR_TIM3EN;
     RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
@@ -23,12 +35,15 @@
     PWM init avec TIM3 freq 10
     Niv_Vibration intique niveau genere par pwm
 */
-/*apres init
-* 
-*
+void Feedback_user_Init (char Voie, char Num_target);
+/*Permet d'envoyer un pic de lumiere
+* En fait, c'est un PWM_set_Cycle cache, avec un log de dans?
 */
-void Feedback_user_Init (char Voie, char Num_target, float Niv_vibration);
-
+void Feedback_user_In(char Voie, float Intensite);
+/*But final:
+* Tourner sans arret un programme qui fait varier rapport cyclique
+* Penser a interruption
+*/
 
 
 #endif 
--- pilotes/src/fbuser.c
+++ pilotes/src/fbuser.c
@@ -1,15 +1,30 @@
 #include "fbuser.h" 
 
-void Feedback_user_Init (char Voie, char Num_target, float Niv_vibration){
+#define FREQ_PWM 5000
+
+void Feedback_user_Init (char Voie, char Num_target){
     if (Voie == 1){
-        Port_IO_Init_Alter_PP(GPIOB,4);
+      Port_IO_Init_Alter_PP(GPIOB,4);
+			PWM_Init(TIM3,1,FREQ_PWM);
     }else if (Voie == 2){
-        Port_IO_Init_Alter_PP(GPIOB,5);
-    }
+      Port_IO_Init_Alter_PP(GPIOB,5);
+			PWM_Init(TIM3,2,FREQ_PWM);
+    }else if (Voie == 3){
+      Port_IO_Init_Alter_PP(GPIOB,8);
+			PWM_Init(TIM4,3,FREQ_PWM);
+		}else if (Voie == 4){
+      Port_IO_Init_Alter_PP(GPIOB,9);
+			PWM_Init(TIM4,4,FREQ_PWM);
+		}
     
-    /*20kHz pour la resolution d'un fichier son*/
-    PWM_Init(TIM3,1,20000);
-    PWM_set_Cycle(TIM3,1,Niv_vibration);
 
 }
-
+void Feedback_user_In(char Voie, float Intensite){
+	float lumino_eye = Intensite*Intensite;
+	if ((Voie==1)||(Voie==2)){
+		PWM_set_Cycle(TIM3, Voie, lumino_eye);
+	}
+	else if ((Voie==3)||(Voie==4)){
+		PWM_set_Cycle(TIM4, Voie, lumino_eye);
+	}
+}
